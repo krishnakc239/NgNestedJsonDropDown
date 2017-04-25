ddbe887f3b3dd8dfe2595c271e013da798e6c95e
prabhatsubedi <krishnakc2339@gmail.com>
2017-04-25 16:44:45 +0545

    asdfasd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 /**
  * Created by kkc on 4/20/17.
  */
-var app=angular.module('myApp',[]);
-    app.controller('taskController',function ($scope) {
+var app = angular.module('myApp', ['angular.filter','infinite-scroll']);
 
-})
