3d6a54b5c87bd8597f0feab82e863aaa860e4d4c
prabhatsubedi <krishnakc2339@gmail.com>
2017-04-25 16:11:12 +0545

    asdfasd

---
+++ js/directive/dropDownDirective.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kkc on 4/20/17.
+ */
+app.directive('dropDownDirective',function () {
+    return{
+        restrict:'EA',
+        templateUrl:'view/dropDownDirective.html',
+        controller:'dropDownController'
+
+    }
+})
