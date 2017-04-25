5477763ea86f13f4de1e034812a06184681af17b
prabhatsubedi <krishnakc2339@gmail.com>
2017-04-25 10:18:04 +0545

    group By date correced

--- js/controller/filterController.js
+++ js/controller/filterController.js
@@ -1,63 +1,28 @@
 /**
  * Created by kkc on 4/22/17.
  */
-angular.module('myApp').controller('filterController',function ($scope,$filter) {
-    $scope.today=new Date();
-    $scope.camelCase= function(){
-       $scope.camelcase= $filter('camelcaseFilter')($scope.my_text);
+app.controller('filterController',function ($scope,$filter) {
+    $scope.today = new Date();
+    $scope.camelCase = function () {
+        $scope.camelcase = $filter('camelcaseFilter')($scope.my_text);
     }
-    $scope.dateDifferrence=function () {
-
-        var DateDiff = {
-            inHour: function (d1, d2) {
-                var t2 = d2.getTime();
-                var t1 = d1.getTime();
-                return parseInt((t2-t1)*60/(24*3600*1000));
-                console.log(parseInt(t2-t1))
-
-            },
-
-            inDays: function(d1, d2) {
-                var t2 = d2.getTime();
-                var t1 = d1.getTime();
-
-                return parseInt((t2-t1)/(24*3600*1000));
-            },
-
-            inWeeks: function(d1, d2) {
-                var t2 = d2.getTime();
-                var t1 = d1.getTime();
-
-                return parseInt((t2-t1)/(24*3600*1000*7));
-            },
-
-            inMonths: function(d1, d2) {
-                var d1Y = d1.getFullYear();
-                var d2Y = d2.getFullYear();
-                var d1M = d1.getMonth();
-                var d2M = d2.getMonth();
-
-                return (d2M+12*d2Y)-(d1M+12*d1Y);
-            },
-
-            inYears: function(d1, d2) {
-                return d2.getFullYear()-d1.getFullYear();
-            }
-        }
-        DateDiff.inHour(d1,d2);
-
-        // var dString = "May, 20, 1984";
-
-        var d1 = new Date($scope.userDate);
-        var d2 = new Date();
-
-        console.log("<br />Number of <b>days</b> since "+$scope.userDate+": "+DateDiff.inDays(d1, d2));
-        console.log("<br />Number of <b>weeks</b> since "+$scope.userDate+": "+DateDiff.inWeeks(d1, d2));
-        console.log("<br />Number of <b>months</b> since "+$scope.userDate+": "+DateDiff.inMonths(d1, d2));
-        console.log("<br />Number of <b>years</b> since "+$scope.userDate+": "+DateDiff.inYears(d1, d2));
-
+    $scope.dateDifferrence = function (date) {
+        $scope.differnce = $filter('dateDiffFilter')($scope.date)
 
     }
-
+    $scope.array = [
+        {date: "2013-09-01 18:02:00", event: "dinner"},
+        {date: "2013-09-01 18:01:00", event: "snacks"},
+        {date: "2013-09-01 10:00:00", event: "lunch"},
+        {date: "2013-09-01 06:00:00", event: "brush"},
+        {date: "2013-09-02 10:01:00", event: "lunch"},
+        {date: "2013-09-02 18:03:00", event: "dinner"},
+        {date: "2013-09-03 16:00:00", event: "snacks"},
+        {date: "2013-09-03 18:00:00", event: "dinner"},
+        {date: "2013-09-04 16:00:00", event: "snacks"},
+        {date: "2013-09-04 06:00:00", event: "brush"},
+        {date: "2013-09-04 10:01:00", event: "lunch"},
+        {date: "2013-09-04 18:03:00", event: "dinner"},
+    ];
 
 })
--- js/directive/filterDirective.js
+++ js/directive/filterDirective.js
@@ -1,7 +1,7 @@
 /**
  * Created by kkc on 4/22/17.
  */
-angular.module('myApp').directive('filterDirective',function () {
+app.directive('filterDirective',function () {
     return{
         restrict:'EA',
         templateUrl:'view/filterDirective.html',
