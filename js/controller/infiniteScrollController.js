6f1b6fc15a375f74e3fb95c8adb32d22c87f4b53
prabhatsubedi <krishnakc2339@gmail.com>
2017-04-25 16:46:48 +0545

    asdfasd

--- js/controller/infiniteScrollController.js
+++ js/controller/infiniteScrollController.js
@@ -1,3 +1,18 @@
 /**
  * Created by kkc on 4/25/17.
  */
+app.controller('infiniteScrollController', function ($scope) {
+    $scope.infiniteList = [];
+    $scope.incr = 1;
+
+    $scope.scrollTriggered = "";
+
+    $scope.loadMore = function(){
+
+        $scope.scrollTriggered += " Scroll Triggered"
+        for(var i = 0; i< 100; i++){
+            $scope.infiniteList.push("Item " + $scope.incr);
+            $scope.incr +=1;
+        }
+    };
+});
--- js/directive/infiniteScrollDirective.js
+++ js/directive/infiniteScrollDirective.js
@@ -1,3 +1,10 @@
 /**
  * Created by kkc on 4/25/17.
  */
+app.directive('infiniteScrollDirective', function () {
+    return{
+        restrict :'EA',
+        templateUrl: 'view/infiniteScrollDirective.html',
+        controller :'infiniteScrollController'
+    }
+})
